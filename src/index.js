dd0e7149de5fe6480fe86ae8f09d2490da3ce300
Manoel-Matias-bls <manoelmatias.cardoso@gmail.com>
2019-08-15 08:48:42 -0300

    Reactotron Configurado e funcionando.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -13,7 +15,9 @@ const styles = StyleSheet.create({
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World.</Text>
+      <Text>Hello World! Welcome the MATC Inc</Text>
+      <Text>Hello World! Welcome the MATC Inc</Text>
+      <Text>Hello World! Welcome the MATC Inc</Text>
     </View>
   );
 };
